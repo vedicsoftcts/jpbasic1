b2724ba8c4f3494c3156fd5976dbebb4e61e68b3
latif, vedic <vedicsoftcts@gmail.com>
2019-08-27 16:07:56 +0530

    jp if addred

---
+++ jpif1.java
@@ -0,0 +1,11 @@
+public class jpif1 {
+
+    public static void main(String[] args) {
+       int a,b;
+       a = 20;
+       b = 15;
+       if (a>b)
+       {
+           System.out.println("Biggest is " +a);
+       }
+    }
