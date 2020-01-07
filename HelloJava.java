e1a3de9ab75419bd44b9fa166a584a5f58bbd1f0
prasad <prasad@gmail.com>
2020-01-07 15:16:24 +0530

    prasad2

--- HelloJava.java
+++ HelloJava.java
@@ -1,9 +1,7 @@
-class HelloJava{
-<<<<<<< HEAD
-	public static void main (String[] args){
-=======
-	public static void main(String[] args){
->>>>>>> d238efa2d37f8648c32b31b59e467ee296f0632a
-		System.out.println("Hello, Java with Jenkins");
+class helloJava{
+              public static void main(String[] args){
+                     System.out.println("Hello, java with jenkins");
+
+
 }
 }
