379dcde62a9e7388835a721214700d695848774b
Deepak <deepak@vedicsoftcts.com>
2020-01-07 15:13:13 +0530

    Deepak java

--- HelloJava.java
+++ HelloJava.java
@@ -1,9 +1,5 @@
 class HelloJava{
 	public static void main(String[] args){
-<<<<<<< HEAD
 		System.out.println("Hello,Java with Jenkins");
-=======
-		System.out.println("Hello, Java with Jenkins");
->>>>>>> d238efa2d37f8648c32b31b59e467ee296f0632a
 }
 }
