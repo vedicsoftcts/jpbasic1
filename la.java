a3950c352219975b6368cfde0c4b3e166ad8d2d6
latif <latif.new@gmail.com>
2020-08-25 13:11:20 +0530

    la file added by latif

---
+++ la.java
@@ -0,0 +1,13 @@
+public class jpif1 {
+
+    public static void main(String[] args) {
+       int a,b;
+       a = 20;
+       b = 15;
+       System.out.println("Hello, I am Latif");
+	if (a>b)
+       {
+           System.out.println("Biggest is " +a);
+       }
+    }
+}
