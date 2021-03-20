0c8e6b00a29f258d27fcc996ea77a63402a91677
latif vedic <latif@vedic.com>
2021-03-20 12:45:20 +0530

    code modified

--- jjif.java
+++ jjif.java
@@ -3,10 +3,14 @@ public class jjif {
     public static void main(String[] args) {
        int a,b;
        a = 20;
-       b = 15;
+       b = 25;
        if (a>b)
        {
            System.out.println("Biggest is " +a);
        }
+	else
+	{
+	 System.out.println("Biggest is " +b);
+	}
     }
 }
