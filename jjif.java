c5af5ef3ec9cbd9ee3ab2e55e260ff66b534277b
latif vedic <latif@vedic.com>
2021-03-20 12:38:42 +0530

    new code1

---
+++ jjif.java
@@ -0,0 +1,12 @@
+public class jjif {
+
+    public static void main(String[] args) {
+       int a,b;
+       a = 20;
+       b = 15;
+       if (a>b)
+       {
+           System.out.println("Biggest is " +a);
+       }
+    }
+}
